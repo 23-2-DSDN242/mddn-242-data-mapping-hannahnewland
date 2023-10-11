484cb4400c313f4f9bc63f52526eaa84d6d9723f
hannahnewland <139179132+hannahnewland@users.noreply.github.com>
2023-10-12 09:30:23 +1300

    Added Bokeh Effect

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
 
 // change these three lines as appropiate
 let sourceFile = "input_2.jpg";
@@ -21,28 +20,98 @@ function setup () {
   background(0, 0, 128);
   sourceImg.loadPixels();
   maskImg.loadPixels();
+  colorMode(HSB);
 }
 
-function draw () {
-  let j = renderCounter;
-  // get one scanline
-  for(let i=0; i<1920; i++) {
-    let pix = sourceImg.get(i, j);
-    let mask = maskImg.get(i, j);
-    if(mask[0] > 128) {
-      // draw the full pixels
-      set(i, j, pix);
-    }
-    else {
-      // draw a "dimmed" version in gray
-      let gray_color = 64 + pix[1] / 8;
-      set(i, j, gray_color);
+let X_STOP = 1920;
+let Y_STOP = 1080;
+let DIAMETER = 15;
+
+// return a custom pixel kernel with given diameter (should be odd). will return array[diameter][diameter]
+function makePixelKernel(diameter, is_reverse=false, is_diamond=false) {
+  let kernel = [];
+  let on_value = 1;
+  let off_value = 0;
+  if (is_reverse) {
+    on_value = 0;
+    off_value = 1;
+  }
+  for (let j=0; j<diameter; j++) {
+    let j_center = (diameter-1) / 2;
+    let j_offset = abs(j_center-j);
+    let j_frac = j_offset / j_center;
+    let row = [];
+    kernel.push(row);
+    for (let i=0; i<diameter; i++) {
+      let i_center = (diameter-1) / 2;
+      let i_offset = abs(i_center-i);
+      let i_frac = i_offset / i_center;
+      if (is_diamond) {
+        if (i_frac + j_frac > 1) {
+          row.push(off_value);
+        }
+        else {
+          row.push(on_value);
+        }
+      }
+      else {
+        if ((i_frac*i_frac + j_frac*j_frac) > 1) {
+          row.push(off_value);
+        }
+        else {
+          row.push(on_value);
+        }
+      }
     }
   }
+  return kernel;
+}
+
+let renderCounter=5;
+function draw () {
+  // make kernel
+  is_reverse = true;
+  is_diamond = false;
+  let kernel = makePixelKernel(DIAMETER, is_reverse, is_diamond)
+
+  let num_lines_to_draw = 40;
+  // get one scanline
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+    for(let i=5; i<X_STOP; i++) {
+      colorMode(RGB);
+      let pix = [0, 0, 0, 255];
+      let mask = maskImg.get(i, j);
+      if (mask[1] > 128) {
+        pix = sourceImg.get(i, j);
+      }
+      else {
+        let sum_rgb = [0, 0, 0]
+        let num_cells = 0;
+        for(let wx=0;wx<DIAMETER;wx++){
+          for (let wy=0;wy<DIAMETER;wy++) {
+            let kernel_value = kernel[wx][wy];
+            if (kernel_value > 0) {
+              let pix = sourceImg.get(i+wx, j+wy);
+              for(let c=0; c<3; c++) {
+                sum_rgb[c] += pix[c];
+              }
+              num_cells += 1;              
+            }
+          }
+        }
+        for(let c=0; c<3; c++) {
+          pix[c] = int(sum_rgb[c] / num_cells);
+        }        
+      }
+
+      set(i, j, pix);
+    }
+  }
+  renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
-  renderCounter = renderCounter + 1;
-  print(renderCounter);
-  if(renderCounter > 1080) {
+
+  // print(renderCounter);
+  if(renderCounter > Y_STOP) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
